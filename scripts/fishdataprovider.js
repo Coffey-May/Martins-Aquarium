cd5bc78b9b4f17c78ff04a3cb4227dd63e5e7a6e
Coffey May <jcoffeymay6@gmail.com>
2019-11-22 10:53:35 -0600

    saving martins aquarium before destruction

---
+++ scripts/fishdataprovider.js
@@ -0,0 +1,55 @@
+const fishCollection = [
+    {
+        name: "cutefish",
+        species: "Goldfish",
+        location: "Atlantic",
+        length: "40 ft",
+        food: "Flakes",
+        image: "fish.jpg"
+    },
+    {
+        name: "blobby",
+        species: "Pirahna",
+        location: "Brazil",
+        length: "3 in",
+        food: "Algae, Crustaceans",
+        image: "fish.jpg"
+    },
+    {
+        name: "Beta",
+        species: "Pirahna",
+        location: "Brazil",
+        length: "3 in",
+        food: "Algae, Crustaceans",
+        image: "fish.jpg"
+    },
+    {
+        name: "cutefish",
+        species: "Pirahna",
+        location: "Brazil",
+        length: "3 in",
+        food: "Algae, Crustaceans",
+        image: "fish.jpg"
+    },
+    {
+        name: "blobby",
+        species: "Pirahna",
+        location: "Brazil",
+        length: "3 in",
+        food: "Algae, Crustaceans",
+        image: "fish.jpg"
+    },
+    {
+        name: "fishyboi",
+        species: "guppy",
+        location: "ponds",
+        length: "3 miles",
+        food: "Algae, Crustaceans",
+        image: "fish.jpg"
+    },
+]
+
+// This is new code. Add this.
+export const useFish = () => {
+    return fishCollection
+}
--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,18 @@
 import initializeDetailButtonEvents from './dialogs.js'
 import addMouseOverEventListeners from './highlight.js'
 import borderColorChange from './tipevents.js'
+import { useFish } from './fishdataprovider.js'
 
 initializeDetailButtonEvents() 
 
 initializeDetailButtonEvents()
 addMouseOverEventListeners() 
 addMouseOverEventListeners()
+const allOfTheFish = useFish()
+
+console.log("all of the fish:", allOfTheFish)
+
+
+for (const fish of allOfTheFish) {
+    console.log("individual fish object:", fish)
+}
